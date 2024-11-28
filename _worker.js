79e00ecb4ff92aa0db49ed7f8a44d92fa43ad960
Sepehr Mohammadi <sepehr.moh81@gmail.com>
2024-11-29 02:59:58 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
     async fetch(request, env) {
                         const url = new URL(request.url);
-                        url.hostname = env.HOST || 'ictfz.ir';
+                        url.hostname = env.HOST || 'motherfuckingwebsite.com';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return fetch(request);      
